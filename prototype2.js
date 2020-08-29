77f646b1293859ad1b8e9015e0acd868e99f56ac
Francish Gautam <gautamfrancish@gmail.com>
2020-08-29 08:25:09 +0545

    ''

--- prototype2.js
+++ prototype2.js
@@ -10,3 +10,20 @@ function particle(x, y) {
 var matter = new particle(100, 50);
 
 console.log(matter.show());
+
+var person = {
+  name: "Francish",
+  sirName: "Gautam",
+  fun: () => {
+    console.log("Swiper no swiping");
+  },
+};
+console.log(person.hasOwnProperty("name"));
+
+//..........................
+
+console.log("hasOwnProperty" in person);
+
+object = Object.create({});
+object.Hello = "There";
+console.log(object.Hello);
